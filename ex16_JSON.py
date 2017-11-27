bcb62648a97714cedcb910282f8a9ce7f9418867
gavin <gavinj84@gmail.com>
2017-11-27 14:02:51 +1100

    Code Basics Python Exercises Continued

---
+++ ex16_JSON.py
@@ -0,0 +1,30 @@
+#!python3
+
+"""Python Exercise with JSON... """
+
+__author__ = "Gavin Jones"
+
+import json
+
+# Dictionary Example as JSON
+# Prefers dict() over {} for a blank dictionary
+book = dict()
+
+book["tom"] = {
+    "name": "tom",
+    "address": "1 red street, NY",
+    "phone": 9898989898
+}
+
+book["bob"] = {
+    "name": "bob",
+    "address": "1 green street, NY",
+    "phone": 9898989897
+}
+
+jsonDump = json.dumps(book)
+print(jsonDump)
+
+# Write the Dump to a File:
+with open("book.txt", "w") as file:
+    file.write(jsonDump)
